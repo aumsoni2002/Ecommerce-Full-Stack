b4ef429800412d75f424dc0e3e065a7c1d983dc5
Aum <aumsoni2002@gmail.com>
2024-03-13 11:50:41 -0400

    Project Progress: Sprint 3 - Security - Member Access Page - Protected Routes - Complete

--- ecommerceFE/src/app/app-routing.module.ts
+++ ecommerceFE/src/app/app-routing.module.ts
@@ -1,15 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injector, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 import { LoginComponent } from './components/login/login.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OktaAuth } from '@okta/okta-auth-js';
+function sendToLoginPage(OktaAuth: OktaAuth, injector: Injector) {
+  // Making use of injector to access any service available within your application
+  const router = injector.get(Router);
+
+  // Redirect the user to custom login page, if the user is not authenticated
+  router.navigate(['/login']);
+}
+
 const routes: Routes = [
   // Below are the list of routes that we will use to create new instance of component once hit
+  {
+    path: 'members',
+    component: MembersPageComponent,
+    canActivate: [OktaAuthGuard],
+    data: { onAuthRequired: sendToLoginPage },
+  },
   { path: 'login/callback', component: OktaCallbackComponent },
-  {path: 'login', component: LoginComponent},
+  { path: 'login', component: LoginComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'cart-details', component: CartDetailsComponent },
   { path: 'products/:id', component: ProductDetailsComponent },
--- ecommerceFE/src/app/app.module.ts
+++ ecommerceFE/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { LoginStatusComponent } from './components/login-status/login-status.com
 import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
 import { OktaAuth } from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = myAppConfig.oidc;
 const oktaAuth = new OktaAuth(oktaConfig);
@@ -38,6 +39,7 @@ const oktaAuth = new OktaAuth(oktaConfig);
     CheckoutComponent,
     LoginComponent,
     LoginStatusComponent,
+    MembersPageComponent,
   ],
   imports: [
     BrowserModule,
