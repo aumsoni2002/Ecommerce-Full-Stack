60be75e87ed8c54dc936cc53d398113c073e1a3c
Aum <aumsoni2002@gmail.com>
2024-04-15 15:01:56 -0400

    Final Commit: Project Completed and Pushed

---
+++ MySQL Database/db-scripts/06-refactor-database-to-add-unique-email.sql
@@ -0,0 +1,16 @@
+USE `ecommerce_db`;
+
+-- Step 1: clean up previous database tables
+
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE customer;
+TRUNCATE orders;
+TRUNCATE order_item;
+TRUNCATE address;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+-- Step 2: make the email address unique
+
+ALTER TABLE customer ADD UNIQUE(email);
